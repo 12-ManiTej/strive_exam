80932a335dc7aae6811a84f72b7993649e0d4051
12-ManiTej <121246541+12-ManiTej@users.noreply.github.com>
2024-11-07 18:36:21 +0530

    Create json2html.js

---
+++ json2html.js
@@ -0,0 +1,23 @@
+export default function json2html(data) {
+  const headers = Array.from(new Set(data.flatMap(Object.keys)));
+
+  let html = `<table data-user="manitejjilla@gmail.com">\n`;
+
+  html += "  <thead>\n    <tr>";
+  headers.forEach(header => {
+    html += `<th>${header}</th>`;
+  });
+  html += "</tr>\n  </thead>\n";
+
+  html += "  <tbody>\n";
+  data.forEach(row => {
+    html += "    <tr>";
+    headers.forEach(header => {
+      html += `<td>${row[header] || ""}</td>`;
+    });
+    html += "</tr>\n";
+  });
+  html += "  </tbody>\n</table>";
+
+  return html;
+}
